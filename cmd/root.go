9cdc2493fdaac68f6f13bba20fe6fc1bece68c5a
Marc Fornos <marc.fornos@gmail.com>
2024-08-09 17:47:13 +0200

    fix root usage

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "steward [assets or chains]",
+	Use:   "steward",
 	Short: "Ocelloids Data Steward Agent command-line interface",
 }
 
